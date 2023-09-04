cd491f43ad96ef18ea53bf5400b7734ba55071d8
V_Wurunxin <v_wurunxin@baidu.com>
2023-09-04 17:53:40 +0800

    nil对比测试

---
+++ grammar/nil.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func nilTest() {
+	a := new([]string)
+	b := new(map[string]string)
+	c := new([]string)
+	fmt.Println(a, b, c, *a, *b, *c, a == nil, b == nil, a == c)
+
+	var d []string
+	var e map[string]string
+	var f interface{}
+	f = d
+	fmt.Println(d, e, d == nil, e == nil, f == nil)
+
+	var i *int
+	var j interface{}
+	fmt.Println(i, j, i == nil, j == nil, i == j)
+
+}
+
+func main() {
+	nilTest()
+}
