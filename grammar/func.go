f36584a3a690e4b3f0bba591906ac1476a1a93b8
V_Wurunxin <v_wurunxin@baidu.com>
2023-09-04 19:38:56 +0800

    结构体func调用测试

---
+++ grammar/func.go
@@ -0,0 +1,25 @@
+package main
+
+type T string
+
+func (t *T) echo() {
+	println("hello")
+}
+
+func funcTest() {
+	var t1 T = "ABC"
+	t1.echo()
+	// 编译不通过
+	// map对象中的元素无法寻址
+	//a := map[string]T{
+	//	"test": "ABC",
+	//}
+	//a["test"].echo()
+	// 常量无法寻址
+	//const b T = "ABC"
+	//b.echo()
+}
+
+func main() {
+	funcTest()
+}
