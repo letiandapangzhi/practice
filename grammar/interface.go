532b35ebbe5256c3aa76987d422b5315f92f501c
V_Wurunxin <v_wurunxin@baidu.com>
2023-09-11 19:31:51 +0800

    interface对比

---
+++ grammar/interface.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+type People interface {
+	Show()
+}
+
+type Student struct{}
+
+func (stu *Student) Show() {
+
+}
+
+func live() People {
+	var stu *Student // nil
+	return stu
+	// People = stu
+}
+
+func main() {
+	if live() == nil {
+		fmt.Println("AAAAAAA")
+	} else {
+		// People为iface，只是data为nil，整体不等于nil
+		fmt.Println("BBBBBBB")
+	}
+	// BBBBBBB
+
+	var iTest People
+	if iTest == nil {
+		fmt.Println("CCCCCC")
+	}
+}
