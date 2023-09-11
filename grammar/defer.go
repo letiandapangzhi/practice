7237fa814ac9d8527c498639408b8b01cde141d8
V_Wurunxin <v_wurunxin@baidu.com>
2023-09-11 17:00:47 +0800

    defer补充

--- grammar/defer.go
+++ grammar/defer.go
@@ -25,10 +25,29 @@ func deferTest1() (i int) {
 	return i
 }
 
+func deferTest3(i, v int) int {
+	fmt.Println(i)
+	return i + 1
+}
+
+type TestType struct{}
+
+func (t *TestType) deferTest(i int) *TestType {
+	fmt.Println(i)
+	return t
+}
+
 func main() {
 	a := deferTest()
 	fmt.Println("返回结果", a) // 0
 
 	a1 := deferTest1()
 	fmt.Println("返回结果", a1) // 1
+
+	defer deferTest3(1, deferTest3(2, 0)) // 2 1
+	// 压栈一次，为了得到第二个参数，需要先计算出deferTest3(2, 0)返回值
+
+	a2 := TestType{}
+	defer a2.deferTest(3).deferTest(4) // 3 4
+	// 压栈一次，为了得到第二个函数，需要先计算出a2.deferTest(3)返回值
 }
