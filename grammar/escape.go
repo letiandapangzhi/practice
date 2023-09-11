3d8500ef24185362484332754d4f4a2e0ff78c76
V_Wurunxin <v_wurunxin@baidu.com>
2023-09-11 16:19:52 +0800

    逃逸补充/make、new区别

--- grammar/escape.go
+++ grammar/escape.go
@@ -8,15 +8,18 @@ func main() {
 	// go build -gcflags="-m" escape.go
 	// 局部变量通常分配在栈上，当编译器分析到外部存在引用会内存逃逸分配到堆上
 	// 返回引用类型会出现内存逃逸，返回值类型不会，个人理解是值类型是一个拷贝
-	string1 := escapeTest1()
-	strings.Split(string1, ",")
+	//string1 := escapeTest1()
+	//strings.Split(string1, ",")
+	//
+	//escapeTest2()
+	//
+	//map1 := escapeTest3()
+	//if _, exist := map1["test"]; exist {
+	//
+	//}
 
-	escapeTest2()
-
-	map1 := escapeTest3()
-	if _, exist := map1["test"]; exist {
-
-	}
+	slice1 := make([]string, 100000)
+	println(slice1)
 }
 
 func escapeTest1() string {
---
+++ grammar/make_new.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func makeNewTest() {
+	a := make([]int, 0, 5)
+	b := new([]int)
+	fmt.Println(a, *b, a == nil, *b == nil)
+
+	c := make(map[string]string)
+	d := new(map[string]string)
+	fmt.Println(c, *d, c == nil, *d == nil)
+}
+
+func main() {
+	makeNewTest()
+}
