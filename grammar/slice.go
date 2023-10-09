9c3c191ed16b23ef97ed8d1bfae1efd8081b0273
大胖纸 <rune@dapangzhideMacBook-Pro.local>
2023-10-09 21:20:01 +0800

    切片测试

---
+++ grammar/slice.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{1, 2, 3, 4}
+	b := a[:1] // [0:1]
+	c := a[1:] // [1:4]
+	fmt.Printf("a len:%d cap:%d\n", len(a), cap(a))
+	fmt.Printf("b len:%d cap:%d\n", len(b), cap(b)) // 0-1=1 4-0=4
+	fmt.Printf("c len:%d cap:%d\n", len(c), cap(c)) // 4-1=1 4-0=4
+}
